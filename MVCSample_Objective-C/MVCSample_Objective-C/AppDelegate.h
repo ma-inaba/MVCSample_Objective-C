9b0568b7ec2b50c3ce2edfb793b63a66952bdddc
inaba masaya <inabamasaya0506@gmail.com>
2015-11-19 19:18:48 +0900

    first commit
    This is first commit

---
+++ MVCSample_Objective-C/MVCSample_Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVCSample_Objective-C
+//
+//  Created by inaba masaya on 2015/11/19.
+//  Copyright © 2015年 inaba masaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVCSample_Objective-C/MVCSample_Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVCSample_Objective-C
+//
+//  Created by inaba masaya on 2015/11/19.
+//  Copyright © 2015年 inaba masaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
